bf051bcacd539b5cb37fa349ba85ef5bea1e9b4a
BatmanZoro <adrielalvarez1999@gmail.com>
2019-02-01 00:29:06 -0500

    materialize editing

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $('[data-toggle=offcanvas]').click(function() {
       $('.row-offcanvas').toggleClass('active');
     });
+
+    // activates materialized drop down menu fuctionality//
+    $(".dropdown-trigger").dropdown();
+
+    
   });
